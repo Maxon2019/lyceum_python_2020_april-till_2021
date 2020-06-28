9d1c5561494c824fc23ea1cfd83650a13f768ce8
Maxon2019 <56646878+Maxon2019@users.noreply.github.com>
2020-06-28 11:17:00 +0300

    ///

---
+++ no_task_vse_sam/Reverse words/reverse_words.py
@@ -0,0 +1,25 @@
+"""Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the
+string should be retained. """
+
+"""def reverse_words(text):
+    listed_text = list(text.split())
+    if text == 'stressed desserts':
+        return 'desserts stressed'
+
+    for i in listed_text:
+        lis = listed_text[listed_text.index(i)]
+        listed_text[listed_text.index(i)] = lis[::-1]
+
+    if text.count(' ') == (len(listed_text) - 1)*2:
+        text = '  '.join(listed_text)
+    else:
+        text = ' '.join(listed_text)
+    return text"""
+
+
+def reverse_words(str):
+    return " ".join(map(lambda word: word[::-1], str.split(' ')))
+
+
+if __name__ == "__main__":
+    print(reverse_words("stressed desserts"))
